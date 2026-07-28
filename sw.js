1621ea4acd9e2532d593a95bb32f758d12df4ab9
kba1008 <sekolah-4896@moe-dl.edu.my>
2026-07-29 00:07:05 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Service Worker - AtletTrack PWA
    Cache hanya untuk fail aplikasi (shell). SEMUA DATA sentiasa diambil
    terus (network only) daripada Google Sheet melalui Apps Script. */
-const CACHE = "atlettrack-v16";
+const CACHE = "atlettrack-v17";
 const SHELL = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
